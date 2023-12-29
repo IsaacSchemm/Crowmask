aa08ba1537b10719ca2432991b3c582012f6466e
Isaac Schemm <isaacschemm@gmail.com>
2023-12-28 21:12:36 -0600

    Condense Weasyl code into one file

--- Crowmask.Weasyl/WeasylClient.cs
+++ Crowmask.Weasyl/WeasylClient.cs
@@ -1,7 +1,64 @@
-﻿using System.Net.Http.Json;
+﻿using Microsoft.FSharp.Collections;
+using System.Net.Http.Json;
 
 namespace Crowmask.Weasyl
 {
+    public record WeasylMediaFile(
+        int? mediaid,
+        string url);
+
+    public record WeasylUserBase(
+        string login,
+        int userid);
+
+    public record WeasylUserMedia(
+        FSharpList<WeasylMediaFile> avatar);
+
+    public record WeasylStatistics(
+        int submissions);
+
+    public record WeasylUserInfo(
+        int? age,
+        string gender,
+        string location,
+        FSharpMap<string, FSharpList<string>> user_links);
+
+    public record WeasylUserProfile(
+        string username,
+        string full_name,
+        string profile_text,
+        WeasylUserMedia media,
+        string login_name,
+        WeasylStatistics statistics,
+        WeasylUserInfo user_info,
+        string link);
+
+    public record WeasylSubmissionMedia(
+        FSharpList<WeasylMediaFile> submission,
+        FSharpList<WeasylMediaFile> thumbnail);
+
+    public record WeasylGallerySubmission(
+        DateTime posted_at,
+        int submitid);
+
+    public record WeasylSubmissionDetail(
+        string link,
+        WeasylSubmissionMedia media,
+        string owner,
+        DateTime posted_at,
+        string rating,
+        string title,
+        bool friends_only,
+        FSharpSet<string> tags,
+        int submitid,
+        string subtype,
+        string description);
+
+    public record WeasylGallery(
+        FSharpList<WeasylGallerySubmission> submissions,
+        int? backid,
+        int? nextid);
+
     public class WeasylClient
     {
         private readonly IHttpClientFactory _httpClientFactory;
--- Crowmask.Weasyl/WeasylGallery.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Crowmask.Weasyl {
-	public class WeasylGallery {
-		public IEnumerable<WeasylGallerySubmission> submissions { get; set; }
-		public int? backid { get; set; }
-		public int? nextid { get; set; }
-	}
-}
