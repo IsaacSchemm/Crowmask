ebbb187b9620c75991a3694d83bceeb7fe38ea78
Isaac Schemm <isaacschemm@gmail.com>
2024-01-10 11:56:22 -0600

    Remove ISigningKey interface and just use RemoteActor

--- Crowmask.Library/Remote/RemoteActor.cs
+++ Crowmask.Library/Remote/RemoteActor.cs
@@ -1,22 +1,10 @@
-﻿using Crowmask.Library.Signatures;
-using System.Security.Cryptography;
-
-namespace Crowmask.Library.Remote
+﻿namespace Crowmask.Library.Remote
 {
     public record RemoteActor(
-            string Id,
-            string? Name,
-            string Inbox,
-            string? SharedInbox,
-            string KeyId,
-            string KeyPem) : ISigningKey
-    {
-        Uri ISigningKey.Id => new(KeyId);
-        RSA ISigningKey.GetRsa()
-        {
-            var rsa = RSA.Create();
-            rsa.ImportFromPem(KeyPem);
-            return rsa;
-        }
-    }
+        string Id,
+        string? Name,
+        string Inbox,
+        string? SharedInbox,
+        string KeyId,
+        string KeyPem);
 }
