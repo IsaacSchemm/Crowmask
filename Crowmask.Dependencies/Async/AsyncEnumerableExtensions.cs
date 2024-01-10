2defd9041140cb57d303bab1fb103cfbd58ec7b5
Isaac Schemm <isaacschemm@gmail.com>
2024-01-10 15:31:06 -0600

    Add more documentation

--- Crowmask.Dependencies/Async/AsyncEnumerableExtensions.cs
+++ Crowmask.Dependencies/Async/AsyncEnumerableExtensions.cs
@@ -1,26 +1,63 @@
 ﻿namespace Crowmask.Dependencies.Async
 {
+    /// <summary>
+    /// Provides methods to run multiple asynchronous sequences alongside
+    /// each other.
+    /// </summary>
     public static class AsyncEnumerableExtensions
     {
+        /// <summary>
+        /// An object that reads objects from an asynchronous sequence one at
+        /// a time and stores them until needed.
+        /// </summary>
+        /// <typeparam name="T">The type of an item in the sequence</typeparam>
+        /// <param name="enumerable">The sequence to read from</param>
         private class Worker<T>(IAsyncEnumerable<T> enumerable)
         {
+            /// <summary>
+            /// An enumerator for the provided sequence.
+            /// </summary>
             private readonly IAsyncEnumerator<T> _enumerator = enumerable.GetAsyncEnumerator();
-            private readonly HashSet<T> _buffer = [];
 
+            /// <summary>
+            /// Stores up to one item read from the sequence.
+            /// </summary>
+            private readonly ICollection<T> _buffer = [];
+
+            /// <summary>
+            /// Items read from the sequence that have not yet been removed.
+            /// </summary>
             public IEnumerable<T> Buffer => _buffer;
 
+            /// <summary>
+            /// If the buffer is empty, pulls a new item from the sequence.
+            /// </summary>
             public async Task RefillAsync()
             {
                 if (_buffer.Count == 0 && await _enumerator.MoveNextAsync())
                     _buffer.Add(_enumerator.Current);
             }
 
+            /// <summary>
+            /// Removes an item from the worker's buffer, if it is present.
+            /// </summary>
+            /// <param name="item">The item to remove</param>
             public void Remove(T item)
             {
                 _buffer.Remove(item);
             }
         }
 
+        /// <summary>
+        /// Runs multiple asynchronous sequences alongside each other (not in
+        /// parallel), and yields newer items first (according to the provided
+        /// selector function). If the input sequences are not already sorted
+        /// newest-first, the order of items is undefined.
+        /// </summary>
+        /// <typeparam name="T">The type of an item in the sequence</typeparam>
+        /// <param name="asyncEnumerables">The sequences to combine</param>
+        /// <param name="dateSelector">A function that extracts the date field from each item</param>
+        /// <returns>A single asynchronous sequence with all items combined</returns>
         public static async IAsyncEnumerable<T> MergeNewest<T>(this IEnumerable<IAsyncEnumerable<T>> asyncEnumerables, Func<T, DateTimeOffset> dateSelector)
         {
             IReadOnlyList<Worker<T>> workers = asyncEnumerables
--- Crowmask.Dependencies/Weasyl/WeasylUserClient.cs
+++ Crowmask.Dependencies/Weasyl/WeasylUserClient.cs
@@ -1,5 +1,8 @@
 ﻿namespace Crowmask.Dependencies.Weasyl
 {
+    /// <summary>
+    /// Allows access to Weasyl data, both through the Weasyl API and through the web interface.
+    /// </summary>
     public class WeasylUserClient(IHttpClientFactory httpClientFactory, IWeasylApiKeyProvider apiKeyProvider)
     {
         private readonly WeasylApiClient weasylClient = new(httpClientFactory, apiKeyProvider);
@@ -13,12 +16,23 @@
             return _whoami ??= await weasylClient.WhoamiAsync();
         }
 
+        /// <summary>
+        /// Returns user profile information from the Weasyl API for the logged-in user.
+        /// </summary>
+        /// <returns>A user profile object</returns>
         public async Task<WeasylUserProfile> GetMyUserAsync()
         {
             var whoami = await WhoamiAsync();
             return _userProfile ??= await weasylClient.GetUserAsync(whoami.login);
         }
 
+        /// <summary>
+        /// Returns submission information for the given ID, or null if it
+        /// doesn't exist, wasn't posted by the logged-in user, or is set to
+        /// friends only.
+        /// </summary>
+        /// <param name="submitid">The submission ID</param>
+        /// <returns>A submission detail object, or null</returns>
         public async Task<WeasylSubmissionDetail?> GetMyPublicSubmissionAsync(int submitid)
         {
             var whoami = await WhoamiAsync();
@@ -28,6 +42,14 @@
                 : null;
         }
 
+        /// <summary>
+        /// Returns a gallery page with limited submission information for
+        /// multiple submissions posted by the logged-in user.
+        /// </summary>
+        /// <param name="count">The maximum number of submissions to include</param>
+        /// <param name="nextid">The ID to use when paginating to older items</param>
+        /// <param name="backid">The ID to use when paginating to newer items</param>
+        /// <returns>A gallery page object</returns>
         public async Task<WeasylGallery> GetMyGalleryAsync(int? count = null, int? nextid = null, int? backid = null)
         {
             var whoami = await WhoamiAsync();
@@ -38,6 +60,11 @@
                 backid: backid);
         }
 
+        /// <summary>
+        /// Returns all submissions, with limited information, for the
+        /// logged-in user, newest to oldest.
+        /// </summary>
+        /// <returns>An asynchronous sequence of all submissions</returns>
         public async IAsyncEnumerable<WeasylGallerySubmission> GetMyGallerySubmissionsAsync()
         {
             var gallery = await GetMyGalleryAsync();
@@ -60,6 +87,10 @@
             }
         }
 
+        /// <summary>
+        /// Returns the IDs of all journals posted by the logged-in user.
+        /// </summary>
+        /// <returns>An asynchronous sequence of all journal IDs</returns>
         public async IAsyncEnumerable<int> GetMyJournalIdsAsync()
         {
             var user = await GetMyUserAsync();
@@ -67,6 +98,13 @@
                 yield return uri;
         }
 
+        /// <summary>
+        /// Returns journal entry information for the given ID, or null if it
+        /// doesn't exist, wasn't posted by the logged-in user, or is set to
+        /// friends only.
+        /// </summary>
+        /// <param name="journalid">The journal ID</param>
+        /// <returns>A journal entry object</returns>
         public async Task<JournalEntry?> GetMyJournalAsync(int journalid)
         {
             var whoami = await WhoamiAsync();
