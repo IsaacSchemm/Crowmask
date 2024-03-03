af8a360047f949e0c6bce07d4f3058a8418aa5ea
Isaac Schemm <isaacschemm@gmail.com>
2024-03-02 22:04:34 -0500

    Add atproto code

---
+++ Crowmask.ATProto.Example/Token.cs
@@ -0,0 +1,16 @@
+﻿using Crowmask.ATProto;
+
+public class Token : IAutomaticRefreshCredentials
+{
+    public string Hostname => "bsky.social";
+
+    public string RefreshToken { get; set; } = "";
+
+    public string AccessToken { get; set; } = "";
+
+    public async Task UpdateTokensAsync(IRefreshTokenCredentials newCredentials)
+    {
+        RefreshToken = newCredentials.RefreshToken;
+        AccessToken = newCredentials.AccessToken;
+    }
+}
